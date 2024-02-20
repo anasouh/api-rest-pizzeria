3e63e57ddf1ded8d64db237d4b6aad9599089dee
Anas Ouhdda <anas.ouhdda.etu@univ-lille.fr>
2024-02-20 11:36:04 +0100

    Update tables schemas

--- WEB-INF/pizzeria.sql
+++ WEB-INF/pizzeria.sql
@@ -1,3 +1,10 @@
+drop table if exists contains;
+drop table if exists orders;
+drop table if exists compose;
+drop table if exists pizzas;
+drop table if exists ingredients;
+drop table if exists users;
+
 CREATE TABLE IF NOT EXISTS users (
     uid SERIAL PRIMARY KEY,
     login VARCHAR(255) NOT NULL,
@@ -18,11 +25,11 @@ CREATE TABLE IF NOT EXISTS pizzas (
 );
 
 CREATE TABLE IF NOT EXISTS compose (
-    pid INT REFERENCES pizzas(pid),
-    iid INT REFERENCES ingredients(iid),
-    PRIMARY KEY (pid, iid),
-    CONSTRAINT fk_pizzas FOREIGN KEY (pid) REFERENCES pizzas(pid),
-    CONSTRAINT fk_ingredients FOREIGN KEY (iid) REFERENCES ingredients(iid)
+    pid INT,
+    iid INT,
+    CONSTRAINT pk_compose PRIMARY KEY (pid, iid),
+    CONSTRAINT fk_pizzas FOREIGN KEY (pid) REFERENCES pizzas(pid) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_ingredients FOREIGN KEY (iid) REFERENCES ingredients(iid) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS orders (
@@ -31,10 +38,10 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 CREATE TABLE IF NOT EXISTS contains (
-    oid INT REFERENCES orders(oid),
-    pid INT REFERENCES pizzas(pid),
+    oid INT,
+    pid INT,
     quantity INT NOT NULL,
-    PRIMARY KEY (oid, pid),
-    CONSTRAINT fk_orders FOREIGN KEY (oid) REFERENCES orders(oid),
-    CONSTRAINT fk_pizzas_contains FOREIGN KEY (pid) REFERENCES pizzas(pid)
+    CONSTRAINT pk_contains PRIMARY KEY (oid, pid),
+    CONSTRAINT fk_orders FOREIGN KEY (oid) REFERENCES orders(oid) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_pizzas_contains FOREIGN KEY (pid) REFERENCES pizzas(pid) ON DELETE CASCADE ON UPDATE CASCADE
 );
